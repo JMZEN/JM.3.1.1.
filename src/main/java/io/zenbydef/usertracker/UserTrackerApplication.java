93e2ccad20892a1ab31671ebf1dd9df59836dd28
JMZEN <lamp507@yandex.ru>
2020-11-25 22:36:47 +0300

    Initial commit

---
+++ src/main/java/io/zenbydef/usertracker/UserTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.zenbydef.usertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/zenbydef/usertracker/UserTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zenbydef.usertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
