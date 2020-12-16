083184ccfa551fea7a42c625040d20049a4b9619
JMZEN <lamp507@yandex.ru>
2020-12-16 22:03:35 +0300

    SNAPSHOT 16/12/2020 Frontend: create Admin Panel/get all users for table

--- src/main/resources/static/admin_users.js
+++ src/main/resources/static/admin_users.js
@@ -1,3 +1,15 @@
 fetch('rest/users').then(value => {
     return value.json();
-}).then((data) => console.log(data));
+}).then((users => {
+    for (let user of users) {
+        let temp = `<tr id="row-${user.userId}">
+                    <td>${user.userId}</td>
+                    <td>${user.firstName}</td>
+                    <td>${user.lastName}</td>
+                    <td>${user.age}</td>
+                    <td>${user.email}</td>
+                    <td>${user.rolesAsString}</td>
+                    </tr>`
+        $('#admin-all-users-table').append(temp)
+    }
+}))
--- src/main/resources/static/header_username_roles.js
+++ src/main/resources/static/header_username_roles.js
@@ -3,4 +3,7 @@ fetch("/rest/users/principal")
     .then(user => {
         $('#header_username').append(user.email)
         $('#header_user_roles').append(`${user.rolesAsString}`)
+        if (!`${user.rolesAsString}`.includes('ADMIN')) {
+            $('#admin-navbar-tab').attr('hidden', 'true')
+        }
     });
