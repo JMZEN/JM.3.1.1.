c2e0d7a048f2dc64a398c44fb4a2de5e72dd071b
JMZEN <lamp507@yandex.ru>
2020-12-21 23:38:27 +0300

    Update done#4(User Only)

--- src/main/resources/static/js/add.js
+++ src/main/resources/static/js/add.js
@@ -1,17 +1,31 @@
 $(document).ready(() => {
     $('#btnAddUser').click(() => {
-        addNewUser();
+        addNewUser().then(refreshPage).then(refreshPage);
     });
 
-    function addNewUser() {
+     function addNewUser() {
+        const dfd = new $.Deferred();
+
         const addUserURL = 'rest/users'
         console.log("addNewUser activated")
         const requestOptions = createPOSTRequestBody();
 
-        fetch(addUserURL, requestOptions)
+         fetch(addUserURL, requestOptions)
             .then(response => response.json())
             .then(user => {
-                let temp = `<tr id="row-${userId}">
+                // let temp = createNewUserRow(user);
+                // $('#mainTableBodyUsers').append(temp)
+            }).then(dfd.resolve)
+        return dfd.promise();
+    }
+})
+
+function refreshPage() {
+    document.location.load();
+}
+
+function createNewUserRow(user) {
+    let newUserRow = `<tr id="row-${userId}">
                     <td id="idEdit">${user.userId}</td>
                     <td>${user.firstName}</td>
                     <td>${user.lastName}</td>
@@ -28,13 +42,8 @@ $(document).ready(() => {
                         type="button" data-toggle="modal" data-target=#modalDeleteView>Delete</button>
                     </td>
                 </tr>`
-                $('#mainTableBodyUsers').append(temp)
-                console.log('done row')
-            }).then(function () {
-            showAllUsers()
-        })
-    }
-})
+    return newUserRow;
+}
 
 function createPOSTRequestBody() {
     return {
@@ -54,31 +63,3 @@ function createPOSTRequestBody() {
         headers: {'Content-type': 'application/json; charset=UTF-8'},
     };
 }
-
-// function (response)
-//
-// {
-//     if (response.status == '200') {
-//         showAllUsers();
-//     }
-
-// function createUserTableRow() {
-//     let temp = `<tr id="row-${userId}">
-//                     <td id="idEdit">${user.userId}</td>
-//                     <td>${user.firstName}</td>
-//                     <td>${user.lastName}</td>
-//                     <td>${user.age}</td>
-//                     <td>${user.email}</td>
-//                     <td>${user.rolesAsString}</td>
-//
-//                     <td>
-//                         <button class="btn btn-info" onclick="func('${user.userId}')"
-//                         type="button" data-toggle="modal" data-target=#modalEditView>Edit</button>
-//                     </td>
-//                     <td>
-//                         <button class="btn btn-danger" onclick="showDeleteUser('${user.userId}')"
-//                         type="button" data-toggle="modal" data-target=#modalDeleteView>Delete</button>
-//                     </td>
-//                 </tr>`
-//     $('#mainTableBodyUsers').append(temp)
-// }
