420a9dca7234173831426b870bc046a1d6136f34
JMZEN <lamp507@yandex.ru>
2020-12-22 11:36:27 +0300

    Update NoRedirect

--- src/main/resources/static/js/add.js
+++ src/main/resources/static/js/add.js
@@ -1,44 +1,51 @@
-$(document).ready(() => {
-    $('#btnAddUser').click(() => {
-        addNewUser().then(refreshPage).then(refreshPage);
-    });
+// // $(document).ready(() => { .then(refreshPage)
+$('#btnAddUser').click(() => {
+    addNewUser()
+        // .then(refreshPage)
+        .then(function () {
+        document.getElementById('newUserForm').reset();
+    }).then()
 
-     function addNewUser() {
-        const dfd = new $.Deferred();
-
-        const addUserURL = 'rest/users'
-        console.log("addNewUser activated")
-        const requestOptions = createPOSTRequestBody();
-
-         fetch(addUserURL, requestOptions)
-            .then(response => response.json())
-            .then(user => {
-                // let temp = createNewUserRow(user);
-                // $('#mainTableBodyUsers').append(temp)
-            }).then(dfd.resolve)
-        return dfd.promise();
-    }
 })
 
 function refreshPage() {
-    document.location.load();
+    showAllUsers();
+}
+
+function addNewUser() {
+    const dfd = new $.Deferred();
+    event.preventDefault()
+    const addUserURL = 'rest/users'
+    console.log("addNewUser activated")
+    const requestOptions = createPOSTRequestBody();
+
+    fetch(addUserURL, requestOptions)
+
+        .then(response => response.json())
+        .then(user => {
+            let temp = createNewUserRow(user);
+            $('#mainTableBodyUsers').append(temp)
+        })
+
+        .then(dfd.resolve)
+    return dfd.promise();
 }
 
 function createNewUserRow(user) {
-    let newUserRow = `<tr id="row-${userId}">
+    let newUserRow = `<tr id="row-${user.userId}">
                     <td id="idEdit">${user.userId}</td>
                     <td>${user.firstName}</td>
                     <td>${user.lastName}</td>
                     <td>${user.age}</td>
                     <td>${user.email}</td>
                     <td>${user.rolesAsString}</td>
-                    
-                    <td> 
-                        <button class="btn btn-info" onclick="func('${user.userId}')" 
+
+                    <td>
+                        <button class="btn btn-info" onclick="func('${user.userId}')"
                         type="button" data-toggle="modal" data-target=#modalEditView>Edit</button>
                     </td>
                     <td>
-                        <button class="btn btn-danger" onclick="showDeleteUser('${user.userId}')" 
+                        <button class="btn btn-danger" onclick="showDeleteUser('${user.userId}')"
                         type="button" data-toggle="modal" data-target=#modalDeleteView>Delete</button>
                     </td>
                 </tr>`
@@ -61,5 +68,18 @@ function createPOSTRequestBody() {
             ]
         }),
         headers: {'Content-type': 'application/json; charset=UTF-8'},
+        // success:
     };
 }
+
+
+
+
+//     .then(document.getElementById("btnAddUser").onclick = function () {
+//         //     window.location.hash = "#usersTable";
+//         // })
+//             // .then(window.location.hash = "#usersTable")
+//
+
+//
+//     // });
