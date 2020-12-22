2604ceb0647a73396a285be772c4847ef1616f32
JMZEN <lamp507@yandex.ru>
2020-12-22 15:00:55 +0300

    Delete resources + Changes in app.prop file

--- resources1/static/admin_edit_user_get_roles.js
+++
@@ -1,9 +0,0 @@
-fetch("/rest/users/roles")
-    .then(response => response.json())
-    .then(result => {
-        for (role of result) {
-            let temp = `<option value=${role.nameOfRole}> ${role.nameOfRole} </option>`
-            $('#user-roles-edit').append(temp)
-            $('#newUserRole').append(temp)
-        }
-    })
--- resources1/static/header_username_roles.js
+++
@@ -1,9 +0,0 @@
-fetch("/rest/users/principal")
-    .then(response => response.json())
-    .then(user => {
-        $('#header_username').append(user.email)
-        $('#header_user_roles').append(`${user.rolesAsString}`)
-        if (!`${user.rolesAsString}`.includes('ADMIN')) {
-            $('#admin-navbar-tab').attr('hidden', 'true')
-        }
-    });
